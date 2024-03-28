5bfd9cef79f5a4d0fda11fdfdde22007395d3aa0
Marcelo Michels <marcelo@michels.app>
2024-03-28 16:00:45 -0300

    create check node version

---
+++ scripts/check-nvm.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const nvmrc = fs.readFileSync('.nvmrc', 'utf8').trim();
+const nodeVersion = process.version.replace('v', '');
+if (!nodeVersion.startsWith(nvmrc)) {
+  console.error(
+    `NodeJS version (${nodeVersion}) not compatible with .nvmrc version (${nvmrc}).`,
+  );
+  process.exit(1);
+}
